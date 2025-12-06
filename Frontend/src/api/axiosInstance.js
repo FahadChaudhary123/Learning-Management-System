904f8f44e00266f44d489f1311d96bf0e7c17d4f
Chadhary-123 <fchaudhary043@gmail.com>
2025-12-06 17:23:29 -0600

    Remove local host

--- Frontend/src/api/axiosInstance.js
+++ Frontend/src/api/axiosInstance.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: process.env.REACT_APP_BACKEND_URL ,
+  baseURL: process.env.REACT_APP_BACKEND_URL,
   withCredentials: true, // ✅ enable cookies/session tokens
 });
+console.log("Backend URL:", process.env.REACT_APP_BACKEND_URL);
 
 export const setAuthToken = (token) => {
   if (token) axiosInstance.defaults.headers.common["Authorization"] = `Bearer ${token}`;
